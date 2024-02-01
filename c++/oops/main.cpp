9d8341b36af8f54b94532b68ad9ec4ef65b2c8e0
Srinidhi S <Srinidhiai01@brightskills.com>
2024-02-01 11:29:05 +0530

    added files

---
+++ c++/oops/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+string name;
+/*
+cout<<"Enter your name:"<<endl;
+cin>> name;
+cout<<name<<endl;
+fflush(stdin);
+
+cout<<"Enter your name:"<<endl;
+getline(cin,name);
+cout<<name<<endl;
+
+
+
+
+
+
+string firstname;
+string lastname;
+cin>>firstname>>lastname;
+cout<<firstname +" "+lastname<<endl;
+string fullname=firstname.append(lastname);
+cout<<fullname<<endl;
+
+lastname.push_back('s');
+cout<<lastname<<endl;
+firstname.push_back('ss');
+cout<<firstname;
+
+*/
+string fullname;
+ cin>>fullname;
+ cout<<fullname.length()<<endl;
+ cout<<fullname.size()<<endl;
+
+    return 0;
+}
